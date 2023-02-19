3c14d32fe1a7222d509e5a54009f39aa44713343
juancamilosalazarrestrepo <juancamilosalazarrestrepo@gmail.com>
2023-02-19 18:40:01 -0500

    delete consoles.log

--- src/pages/api/chat.js
+++ src/pages/api/chat.js
@@ -6,7 +6,6 @@ export default async function handler(req, res) {
   }
 
   try {
-    console.log('apikey', process.env.OPENAI_API_KEY)
     const response = await fetch('https://api.openai.com/v1/completions', {
       method: 'POST',
       headers: {
